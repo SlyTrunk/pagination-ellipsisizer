858f1809cc7736ad60f27b20b16561eaed6619db
Ben Dalziel <b.j.dalziel@gmail.com>
2011-09-13 23:02:17 -0700

    Clean up comments

--- Ellipsisizer.php
+++ Ellipsisizer.php
@@ -1,13 +1,31 @@
 <?php
 
-class Ellipsisizer {
+class Ellipsisizer
+{
 
+    /*
+     * @brief A PHP utility to annotate an ordered array of keyed items with pagination ellipsis data
+     *
+     * @param   string  $selected      The key of the currently selected item. N.B. Should be of the form:
+     *                                  - "$itemKeyPrefix . (int)index", for example, "page-4".
+     * @param   array   $listItems     An array of items, indexed by item keys. N.B. Assumes the array is ksorted for 
+     *                                 the items whose keys are prefixed with $itemKeyPrefix.
+     * @param   int     $maxItemCount  The number to items to render from within the list. N.B. The first, selected and 
+     *                                 last items are always rendered. A value >= 3 is therefore recommended.
+     * @param   boolean $jsEnabled     [opt.] Indicates whether or not the data augmentation should include annotations 
+     *                                 that facilitate simple client size ellipsis control.
+     * @param   string  $itemKeyPrefix [opt.] The desired number of items viewable following the introduction of 
+     *                                 ellipses
+     *
+     * @return  array                  The original input array ($listItems), augmented with ellipsis data
+     *
+     */
     public static function ellipsisize($selected, 
                                        $listItems, 
                                        $maxItemCount, 
-                                       $jsEnabled = false, 
-                                       $itemKeyPrefix = "page-") {
-
+                                       $jsEnabled     = false, 
+                                       $itemKeyPrefix = "page-")
+    {
         $pageCount = 0;
         foreach ($listItems as $key => $listItem) {
             if (strpos($key, $itemKeyPrefix) !== false) {
@@ -61,17 +79,18 @@ class Ellipsisizer {
     }
 
     /*
-     * Determines whether the item will be visible, given the indices of the
-     * window of visible nav items surrounding the selected item
+     * Determines whether the item will be visible, given the indices of the window of visible items surrounding the 
+     * selected item
      *
      * @return  boolean
      */
-    protected static function isItemVisible ($itemIndex, $lastItemIndex, 
+    protected static function isItemVisible ($itemIndex, 
+                                             $lastItemIndex, 
                                              $selectedWindowLowerBoundIndex, 
-                                             $selectedWindowUpperBoundIndex) {
-
+                                             $selectedWindowUpperBoundIndex)
+    {
         if ($itemIndex == 0 || $itemIndex == $lastItemIndex) {
-            // First or last item
+            // First or last item - always visible
             return true;
         }
         if ($itemIndex >= $selectedWindowLowerBoundIndex && 
@@ -79,39 +98,40 @@ class Ellipsisizer {
             // In the window surrounding the selected item
             return true;
         }
-
         return false;
     }
 
     /*
-     * Provides the indices describing a window of items surrounding the input item,
-     * useful for inserting ellipses into pagination navigations.
-     * Configurable via the maxItemCount to abbreviate the navigation more or less.
+     * @brief Provides the indices describing a window of items surrounding the item at the inputted index.
      *
-     * @param   int $itemIndex     The index of the item we're inquiring about.
-     *                             Think of this as the index of the seleted item
-     *                             in the list
-     * @param   int $lastItemIndex The index of the last item in the list
-     * @param   int $maxItemCount  The desired number of items viewable following the introduction of ellipses
+     * @param   int $itemIndex     The index of the item whose bounding box we require the indices of. Zero based!
+     * @param   int $lastItemIndex The index of the last item in the list. Zero based!
+     * @param   int $maxItemCount  The desired number of items visible following the introduction of ellipses.
+     *
      * @return  array/boolean      Containing two ints:
      *                              [0] => (int) the index of the lower bound of the window
      *                              [1] => (int) the index of the upper bound of the window
-     *                             Or <bool>false if the data describes a scenario that doesn't
-     *                             need ellipses
+     *                             Or <bool>false if the data describes a scenario that doesn't need ellipses
      */
-    protected static function getItemEllipsisWindowIndices ($itemIndex, $lastItemIndex, $maxItemCount) {
-
+    protected static function getItemEllipsisWindowIndices($itemIndex, 
+                                                           $lastItemIndex, 
+                                                           $maxItemCount)
+    {
         if (!$maxItemCount || $maxItemCount < 3) {
-            // Not supported, we have to have at least 3 items visible
-            // 1st, selected, and last
+            // Not supported, we have to have at least 3 items visible; 1st, selected, and last. The case where the 
+            // selected item is the first or last item is certainly handled, but only makes sense in isolation. For
+            // the sake of consistency, we only support >= 3.
             return false;
         }
         if ($lastItemIndex < $maxItemCount) {
-            // Too few items for ellipsis
+            // Too few items for ellipsis/ellipses
             return false;
         }
 
-        // One for each of: selected item, 1st item, last item
+        // Three deductions, one for each of the:
+        // - First item
+        // - Selected item
+        // - Last item
         $deductions = 3;
         if ($itemIndex == 0 || $itemIndex == $lastItemIndex) {
             // Selected item is the same as the first || last item
@@ -120,14 +140,13 @@ class Ellipsisizer {
 
         $windowSize = $maxItemCount - $deductions;
         $windowSizeToLeftOfSelected  = (int) floor($windowSize / 2);
-        $windowSizeToRightOfSelected = (int)  ceil($windowSize / 2);
+        $windowSizeToRightOfSelected = (int)  ceil($windowSize / 2); // This favors items infront of the selected item
 
         $selectedWindowLowerBoundIndex = $itemIndex - $windowSizeToLeftOfSelected;
         $selectedWindowUpperBoundIndex = $itemIndex + $windowSizeToRightOfSelected;
 
-        // If the window encompasses the first or last item, and the
-        // selected item is neither of those, the size of the window
-        // may grow:
+        // If the window encompasses the first or last item, and the selected item is neither of those, the size of the 
+        // window may grow:
         $windowGrowth = 0;
         if (!($itemIndex == 0 || $itemIndex == $lastItemIndex)) {
             $windowGrowth = (floor(($windowSize + 1) / 2) - $windowSizeToLeftOfSelected) + 
@@ -135,16 +154,14 @@ class Ellipsisizer {
         }
 
         if ($selectedWindowLowerBoundIndex <= 0) {
-            // Add excess to upperbound, and set lower to 0;
-            // The first item is now included in the window
+            // Add growth to upperbound, and set lower to 0. The first item is now included in the window.
             $selectedWindowUpperBoundIndex = 
                 min($lastItemIndex, 
                     $selectedWindowUpperBoundIndex + (0 - $selectedWindowLowerBoundIndex) + $windowGrowth);
             $selectedWindowLowerBoundIndex = 0;
         }
         if ($selectedWindowUpperBoundIndex >= $lastItemIndex) {
-            // Add excess to lowerbound, and set to lastItemIndex
-            // The last item is now included in the window
+            // Add growth to lowerbound, and set to lastItemIndex. The last item is now included in the window.
             $selectedWindowLowerBoundIndex = 
                 max(0, 
                     $selectedWindowLowerBoundIndex - ($selectedWindowUpperBoundIndex - $lastItemIndex) - $windowGrowth);
